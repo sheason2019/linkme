b16d32e961d845e08f138bb88312d181c5e17cb6
sheason2019 <811627877@qq.com>
2022-11-20 23:39:33 +0800

    feat: 会话框锁定底部功能

--- web/src/pages/chat/components/messages/index.tsx
+++ web/src/pages/chat/components/messages/index.tsx
@@ -1,4 +1,5 @@
 import { Box, Link, Stack } from "@mui/material";
+import { useEffect, useRef } from "react";
 import CustomLink from "../../../../common/components/custom-link";
 import useChat from "../../hooks/use-chat";
 import useSocket from "../../hooks/use-socket";
@@ -9,8 +10,51 @@ const Messages = () => {
   const { chat } = useChat();
   const { handlePullMoreMessage } = useSocket();
 
+  // 是否需要锁定在聊天信息的最底部
+  const focusBottomRef = useRef(true);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleToBottom = (element: HTMLDivElement | null) => {
+    if (!element) return;
+
+    element.scrollTo({ top: element.scrollHeight });
+  };
+
+  useEffect(() => {
+    // 当用户切换房间后，自动将聊天内容锁定在页面的最底部
+    focusBottomRef.current = true;
+  }, [chat.currentConv?.Id]);
+
+  useEffect(() => {
+    if (focusBottomRef.current) {
+      handleToBottom(containerRef.current);
+    }
+  }, [chat.messages]);
+
+  // 当页面滚动到最底部时，将focusBottomRef设置为true，否则设置为false
+  useEffect(() => {
+    const element = containerRef.current;
+    if (!element) return;
+
+    const eventHandler = (e: Event) => {
+      const element = e.target as HTMLDivElement;
+      const focusBottom =
+        element.scrollTop + element.offsetHeight + 4 > element.scrollHeight;
+      focusBottomRef.current = focusBottom;
+    };
+    element.addEventListener("scroll", eventHandler);
+    return () => element.removeEventListener("scroll", eventHandler);
+  }, [containerRef]);
+
   return (
-    <Stack flex={1} p={1} spacing={1} sx={{ overflowY: "auto" }}>
+    <Stack
+      ref={containerRef}
+      flex={1}
+      p={1}
+      spacing={1}
+      sx={{ overflowY: "auto" }}
+      component="div"
+    >
       {chat.hasMoreMessage && (
         <Stack alignItems="center">
           <CustomLink
