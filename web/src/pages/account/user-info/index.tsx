2110d578693da8b1cb9b6e8c9cf5825840c04a57
sheason2019 <811627877@qq.com>
2022-11-25 16:22:52 +0800

    feat: 用户信息页面的移动端适配

--- web/src/pages/account/user-info/index.tsx
+++ web/src/pages/account/user-info/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import { useState } from "react";
+import { useCheckMobile } from "../../../common/hooks/use-check-mobile";
 import useUserInfo from "../../../common/hooks/use-user-info";
 import Signature from "./components/signature";
 import UploadAvatar from "./components/upload-avatar";
@@ -7,25 +8,42 @@ import UploadAvatar from "./components/upload-avatar";
 const UserInfoPage = () => {
   const { userInfo } = useUserInfo();
 
-  const [edit, setEdit] = useState(false);
+  const { isMobile } = useCheckMobile();
+
+  if (!isMobile) {
+    return (
+      <>
+        <Container maxWidth="lg" sx={{ mt: 2 }}>
+          <Stack direction="row">
+            <Box sx={{ width: 256 }}>
+              <UploadAvatar />
+              <Typography variant="h5" sx={{ mt: 1 }}>
+                {userInfo.user?.Username}
+              </Typography>
+              <Typography variant="h6" color="GrayText">
+                UID:{userInfo.user?.UserId}
+              </Typography>
+              <Signature />
+            </Box>
+          </Stack>
+        </Container>
+      </>
+    );
+  }
 
   return (
-    <>
-      <Container maxWidth="lg" sx={{ mt: 2 }}>
-        <Stack direction="row">
-          <Box sx={{ width: 256 }}>
-            <UploadAvatar />
-            <Typography variant="h5" sx={{ mt: 1 }}>
-              {userInfo.user?.Username}
-            </Typography>
-            <Typography variant="h6" color="GrayText">
-              UID:{userInfo.user?.UserId}
-            </Typography>
-            <Signature />
-          </Box>
-        </Stack>
-      </Container>
-    </>
+    <Container>
+      <Stack alignItems="center" sx={{ mt: 2 }}>
+        <UploadAvatar />
+        <Typography variant="h5" sx={{ mt: 1 }}>
+          {userInfo.user?.Username}
+        </Typography>
+        <Typography variant="h6" color="GrayText">
+          UID:{userInfo.user?.UserId}
+        </Typography>
+      </Stack>
+      <Signature />
+    </Container>
   );
 };
 
