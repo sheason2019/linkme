4b0152e68aaed074c9474ebe382ebff80cd5b88f
sheason2019 <811627877@qq.com>
2022-11-26 21:11:37 +0800

    fixup: 遗漏的Change

--- web/src/pages/chat/components/main/index.tsx
+++ web/src/pages/chat/components/main/index.tsx
@@ -45,7 +45,7 @@ const Mobile = () => {
       </Stack>
       <Dialog
         fullScreen
-        open={!!chat.currentConv}
+        open={!!chat.currentConv && chat.currentConv.Id === chat.socketConvId}
         TransitionComponent={Transition}
         sx={{
           height: "100%",
