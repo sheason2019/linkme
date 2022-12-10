2d115ae792abe1cc9fd68773700f9d4e7532e782
sheason2019 <811627877@qq.com>
2022-12-11 00:09:15 +0800

    chore: 微调Todo首页样式

--- web/src/pages/todo/components/home/index.tsx
+++ web/src/pages/todo/components/home/index.tsx
@@ -16,13 +16,13 @@ const Home = () => {
   return (
     <Container sx={{ mt: 2 }}>
       <Card sx={{ p: 2 }}>
-        <Stack direction="row" sx={{ verticalAlign: "baseline" }} spacing={2}>
+        <Stack direction="row" sx={{ alignItems: "center" }} spacing={2}>
           <AccountCircleIcon fontSize="large" />
-          <Typography variant="h4">用户概览</Typography>
+          <Box sx={{ fontSize: "1.55rem" }}>用户概览</Box>
         </Stack>
         <Stack direction="row" sx={{ mt: 2 }}>
-          <Stack sx={{ maxWidth: 256 }} spacing={1}>
-            <Avatar sx={{ width: 256, height: 256 }} />
+          <Stack sx={{ maxWidth: 256 }} spacing={2}>
+            <Avatar sx={{ width: 220, height: 220, alignSelf: "center" }} />
             <Typography
               textOverflow="ellipsis"
               sx={{ overflow: "hidden" }}
@@ -36,9 +36,9 @@ const Home = () => {
         </Stack>
       </Card>
       <Card sx={{ mt: 2, p: 2 }}>
-        <Stack direction="row" sx={{ verticalAlign: "baseline" }} spacing={2}>
+        <Stack direction="row" sx={{ alignItems: "center" }} spacing={2}>
           <CommitIcon fontSize="large" />
-          <Typography variant="h4">Commit记录</Typography>
+          <Box sx={{ fontSize: "1.55rem" }}>Commit记录</Box>
         </Stack>
         <Stack
           sx={{ height: 360, color: "GrayText" }}
