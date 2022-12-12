55664cb838dd159eba17f74f309b8ab654cf65d8
sheason <811627877@qq.com>
2022-12-12 14:58:20 +0800

    chore: update web build script

--- web/build.sh
+++ web/build.sh
@@ -1,3 +1,3 @@
 docker build ../ -t sheason/linkme-web -f ./Dockerfile
 
-docker run sheason/linkme-web -v /var/linkme/web:/var/linkme/web
+docker run -v /var/linkme/web:/var/linkme/web sheason/linkme-web 
---
+++ web/src/pages/todo/hooks/use-todo.ts
@@ -0,0 +1,3 @@
+const useTodo = () => {};
+
+export default useTodo;
